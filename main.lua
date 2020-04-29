710fbf5d9e1f7539e0b556642ae88366a7e13372
tiltproofRain <srijannr1@gmail.com>
2020-04-29 17:06:54 +0530

    moved core files into src folder

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
-local Sprite = require("SpriteSheet")
-local Animation = require("anim")
+local Sprite = require("src/SpriteSheet")
+local Animation = require("src/anim")
 
 local img, anim
 local frames = {}
@@ -9,8 +9,10 @@ local currentFrame = 1
 local timer = 0
 
 function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
   img = Sprite:new('images/man.png', 6, 1)
   anim = Animation:new(img, 1, 6, 0.1, true)
+
   -- local hframes, width, height = 6, img:getDimensions()
   -- w = width / hframes
   -- frames[1] = love.graphics.newQuad( 0, 0, w, 32, img:getDimensions())
@@ -22,7 +24,7 @@ end
 function love.draw()
   -- love.graphics.draw(img, frames[currentFrame], pos.x, pos.y, 0, 2, 2)
   -- love.graphics.print(timer)
-  anim:show(pos.x, pos.y, 0,  2.5, 2.5)
+  anim:show(pos.x, pos.y, 0,  4, 4)
 end
 
 
@@ -36,9 +38,9 @@ function love.update(dt)
   --   timer = 0
   -- end
   anim:update(dt)
-  if love.keyboard.isDown('a') then pos.x = pos.x - 4 end
-  if love.keyboard.isDown('d') then pos.x = pos.x + 4 end
-  if love.keyboard.isDown('w') then pos.y = pos.y - 4 end
-  if love.keyboard.isDown('s') then pos.y = pos.y + 4 end
+  if love.keyboard.isDown('a') then pos.x = pos.x - 6 end
+  if love.keyboard.isDown('d') then pos.x = pos.x + 6 end
+  if love.keyboard.isDown('w') then pos.y = pos.y - 6 end
+  if love.keyboard.isDown('s') then pos.y = pos.y + 6 end
 
 end
