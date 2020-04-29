677a78d18539d063c675e32c8aedb5aaf7bdb51c
tiltproofRain <srijannr1@gmail.com>
2020-04-29 16:22:21 +0530

    Can now store spritesheets as grids of frames and index through them.

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local Sprite = require("SpriteSheet")
+
 local img
 local frames = {}
 local pos = {x = 100, y = 100}
@@ -6,30 +8,32 @@ local currentFrame = 1
 local timer = 0
 
 function love.load()
-  img = love.graphics.newImage( 'images/man.png' )
-  local hframes, width, height = 6, img:getDimensions()
-  w = width / hframes
-  frames[1] = love.graphics.newQuad( 0, 0, w, 32, img:getDimensions())
-  frames[2] = love.graphics.newQuad(w, 0, w, 32, img:getDimensions())
-  frames[3] = love.graphics.newQuad(2 * w, 0, w, 32, img:getDimensions())
+  img = Sprite:new('images/man.png', 6, 1)
+  -- local hframes, width, height = 6, img:getDimensions()
+  -- w = width / hframes
+  -- frames[1] = love.graphics.newQuad( 0, 0, w, 32, img:getDimensions())
+  -- frames[2] = love.graphics.newQuad(w, 0, w, 32, img:getDimensions())
+  -- frames[3] = love.graphics.newQuad(2 * w, 0, w, 32, img:getDimensions())
   love.graphics.setBackgroundColor(0.5, 0.5, 0.02)
 end
 
 function love.draw()
-  love.graphics.draw(img, frames[currentFrame], pos.x, pos.y, 0, 2, 2)
-  love.graphics.print(timer)
+  -- love.graphics.draw(img, frames[currentFrame], pos.x, pos.y, 0, 2, 2)
+  -- love.graphics.print(timer)
+  img:showFrame(3, 200, 200)
+  love.graphics.print(table.getn(img._frames))
 end
 
 
 function love.update(dt)
 
-  timer = timer + dt
-  if timer > 0.1 then
-    if currentFrame > 2 then
-      currentFrame = 1
-    else currentFrame = currentFrame + 1 end
-    timer = 0
-  end
+  -- timer = timer + dt
+  -- if timer > 0.1 then
+  --   if currentFrame > 2 then
+  --     currentFrame = 1
+  --   else currentFrame = currentFrame + 1 end
+  --   timer = 0
+  -- end
 
   if love.keyboard.isDown('a') then pos.x = pos.x - 4 end
   if love.keyboard.isDown('d') then pos.x = pos.x + 4 end
