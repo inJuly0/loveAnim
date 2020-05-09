d115f6240ac36ffdafd4d19dad98bb01faa5298d
inJuly <50487716+inJuly0@users.noreply.github.com>
2020-05-09 17:17:18 +0530

    can now create spritesheet from an ImageObject or a path.

--- src/anim.lua
+++ src/anim.lua
@@ -1,12 +1,16 @@
 local SpriteSheet = {}
 
+
 function SpriteSheet:new(imagePath, hFrames, vFrames)
   newSheet = {
     _imageData = {source = nil, rows = 0, cols = 0},
     frameWidth = 0, frameHeight = 0
   }
 
-  newSheet._imageData.source = love.graphics.newImage(imagePath)
+  if type(imagePath) == 'string' then
+    newSheet._imageData.source = love.graphics.newImage(imagePath)
+  else  newSheet._imageData.source = imagePath end
+
   if newSheet._imageData.source == nil then return nil end
 
   -- set spritesheet grid dimensions
@@ -14,7 +18,6 @@ function SpriteSheet:new(imagePath, hFrames, vFrames)
   local width, height = newSheet._imageData.source:getDimensions()
   newSheet.frameWidth = width / hFrames
   newSheet.frameHeight = height / vFrames
-
 
   newSheet._imageData.cols = width  / newSheet.frameWidth
   newSheet._imageData.rows = height / newSheet.frameHeight
@@ -40,6 +43,7 @@ function SpriteSheet:new(imagePath, hFrames, vFrames)
   return setmetatable(newSheet, self)
 end
 
+
 function SpriteSheet:showFrame(frameIndex, x, y, r, sx, sy)
 
   if frameIndex > table.getn(self._frames) or frameIndex <= 0 then
@@ -51,13 +55,14 @@ function SpriteSheet:showFrame(frameIndex, x, y, r, sx, sy)
       x, y, r, sx, sy)
 end
 
+
 -- The animation table contains data about the the 'Frame set' which is a
 -- collection of frames in a spritesheet.
 
 local Animation = {}
 
-function Animation:new(spriteSheet, s, e, t, l)
 
+function Animation:new(spriteSheet, s, e, t, l)
   newAnim = {
     sheet = spriteSheet,
     startIndex = s,
@@ -65,30 +70,33 @@ function Animation:new(spriteSheet, s, e, t, l)
     duration = t,
     loop = l,
     currentIndex = s,
-    frameCount = e - s,
+    frameCount = e - s + 1,
     timeElapsed = 0
   }
 
-  assert(newAnim.frameCount > 0)
+  assert(newAnim.frameCount >= 0)
   self.__index = self
   return setmetatable(newAnim, self)
 end
+
 
 function Animation.newSpriteSheet(path, hFrames, vFrames)
   return SpriteSheet:new(path, hFrames, vFrames)
 end
 
+
 function Animation:update(dt)
   self.timeElapsed = self.timeElapsed + dt
   if self.timeElapsed >= self.duration then
-    if self.currentIndex + 1 > self.frameCount and self.loop then
-      self.currentIndex = self.startIndex
+    if self.currentIndex + 1 > self.frameCount then
+      if self.loop then self.currentIndex = self.startIndex end
     else
       self.currentIndex = self.currentIndex + 1
     end
     self.timeElapsed = 0
   end
 end
+
 
 function Animation:show(x, y, r, sx, sy)
   self.sheet:showFrame(self.currentIndex, x, y, r, sx, sy)
